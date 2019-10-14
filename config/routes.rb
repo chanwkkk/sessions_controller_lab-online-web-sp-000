0ae942500b9efc813749a641166e5d1209522c66
Yingqi Chen <chanwk18@hotmail.com>
2019-10-14 19:34:54 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  #get 'sessions/new'
+  get 'sessions/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'application/hello'
+  root 'application#hello'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
